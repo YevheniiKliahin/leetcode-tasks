319fe0a07a371e118e4664ee11df308f9541cec0
YevheniiKliahin <yevhenii.kliahin@gmail.com>
2023-06-16 18:59:39 +0300

    arrays practice MergeSortedArray fix

--- src/main/java/com/kliahin/arrays/MergeSortedArray.java
+++ src/main/java/com/kliahin/arrays/MergeSortedArray.java
@@ -4,37 +4,22 @@ import java.util.Arrays;
 
 public class MergeSortedArray {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (n == 0) return;
-        if(m == 0){
-            if(n != 0) {
-                if (nums1.length >= nums2.length){
-                    for(int i = 0; i < nums2.length; i++){
-                        nums1[i] = nums2[i];
-                    }
-                }
-                return;
+        int counter = m + n -1;
+        int maxIdx1 = m - 1;
+        int maxIdx2 = n - 1;
+
+        while(maxIdx2 >= 0){
+            if(maxIdx1 >= 0 && nums1[maxIdx1] > nums2[maxIdx2]){
+                nums1[counter] = nums1[maxIdx1];
+                maxIdx1--;
+                counter--;
+            }
+            else{
+                nums1[counter] = nums2[maxIdx2];
+                maxIdx2--;
+                counter--;
             }
         }
-
-        int counter = nums1.length - 2;
-        int maxIdx1 = m-1;
-        int maxIdx2 = n-1;
-
-        while(maxIdx2 >= 0) {
-            if(nums2[maxIdx2] > nums1[maxIdx1]){
-                nums1[counter+1] = nums2[maxIdx2];
-                maxIdx2--;
-            } else if(nums2[maxIdx2] == nums1[maxIdx1]){
-                nums1[counter+1] = nums2[maxIdx2];
-                nums1[counter] = nums1[maxIdx1];
-                maxIdx2--;
-                maxIdx1--;
-            } else if (nums2[maxIdx2] < nums1[maxIdx1]) {
-                nums1[counter+1] = nums1[maxIdx1];
-                maxIdx1--;
-            }
-            counter--;
-            }
     }
 
     public static void main(String[] args) {
@@ -44,13 +29,17 @@ public class MergeSortedArray {
         int[] test22 = new int[]{}; int n2 = 0;
         int[] test31 = new int[]{0}; int m3 = 0;
         int[] test32 = new int[]{1}; int n3 = 1;
+        int[] test41 = new int[]{2, 0}; int m4 = 1;
+        int[] test42 = new int[]{1}; int n4 = 1;
 
         merge(test11, m1, test12, n1);
         merge(test21, m2, test22, n2);
         merge(test31, m3, test32, n3);
+        merge(test41, m4, test42, n4);
 
         System.out.println(Arrays.toString(test11));
         System.out.println(Arrays.toString(test21));
         System.out.println(Arrays.toString(test31));
+        System.out.println(Arrays.toString(test41));
     }
 }
